dd4ccc08e15ec824d7f911679205e0da6c8c7ec8
jaortizgonzalez-cyber <jaortizgonzalez@gmail.com>
2026-08-14 11:42:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,6 @@
 //  Mientras esté sin llenar, los juegos funcionan solo en modo local.
 // ─────────────────────────────────────────────────────────────
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCQ8jiMdIHKwdfUoVj2MFgp8rin0PLwI20",
@@ -19,8 +14,5 @@ const firebaseConfig = {
   messagingSenderId: "731103905413",
   appId: "1:731103905413:web:b963e3c9125790e3878bec"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 export const configurado = !firebaseConfig.apiKey.startsWith("PEGA_");
